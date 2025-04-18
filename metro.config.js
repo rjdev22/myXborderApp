1606bbb073a682cce7a7308fac6e81997ed2755b
rohit <rohit@gmail.com>
2025-04-18 12:36:06 +0530

    resolev conflict issue

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,25 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
 const config = {};
-
+//
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+//module.exports = (async () => {
+//  const {
+//    resolver: { assetExts, sourceExts },
+//  } = await getDefaultConfig();
+//
+//  return {
+//    transformer: {
+//      // Enable svg-transformer for handling SVGs
+//      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+//    },
+//    resolver: {
+//      // Add support for SVG in source files
+//      assetExts: assetExts.filter(ext => ext !== 'svg'), // Remove SVG from assetExts
+//      sourceExts: [...sourceExts, 'svg'], // Add SVG to sourceExts
+//    },
+//  };
+//})();
